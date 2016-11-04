41a114e160cb5487bf1b2f5a9ab6a173a75a9187
Renata Hodovan <reni@inf.u-szeged.hu>
2016-11-04 18:49:48 +0100

    Add missing copyright to setup.py.

--- setup.py
+++ setup.py
@@ -1,3 +1,10 @@
+# Copyright (c) 2016 Renata Hodovan, Akos Kiss.
+#
+# Licensed under the BSD 3-Clause License
+# <LICENSE.md or https://opensource.org/licenses/BSD-3-Clause>.
+# This file may not be copied, modified, or distributed except
+# according to those terms.
+
 from os.path import dirname, join
 from setuptools import setup, find_packages
 

bba0eb960e10352b13bbffa8694616c4e1c8cdbf
Akos Kiss <akiss@inf.u-szeged.hu>
2017-03-30 10:34:46 +0200

    Add byte-flipping fuzzer decorator

---
+++ fuzzinator/fuzzer/byte_flip_decorator.py
@@ -0,0 +1,70 @@
+# Copyright (c) 2017 Renata Hodovan, Akos Kiss.
+#
+# Licensed under the BSD 3-Clause License
+# <LICENSE.rst or https://opensource.org/licenses/BSD-3-Clause>.
+# This file may not be copied, modified, or distributed except
+# according to those terms.
+
+import math
+import random
+
+from ..call import CallableDecorator
+
+
+class ByteFlipDecorator(CallableDecorator):
+    """
+    Decorator to add extra random byte flips to fuzzer results.
+
+    **Mandatory parameter of the decorator:**
+
+      - ``frequency``: the length of the test divided by this integer number
+        gives the number of bytes flipped.
+
+    **Optional parameters of the decorator:**
+
+      - ``min_byte``: minimum value for the flipped bytes (integer number, 32 by
+        default, the smallest ASCII code of the printable characters).
+      - ``max_byte``: maximum value for the flipped bytes (integer number, 126
+        by default, the largest ASCII code of the printable characters).
+
+    **Example configuration snippet:**
+
+        .. code-block:: ini
+
+            [sut.foo]
+            # see fuzzinator.call.*
+
+            [fuzz.foo-with-flips]
+            sut=sut.foo
+            fuzzer=fuzzinator.fuzzer.ListDirectory
+            fuzzer.decorate(0)=fuzzinator.fuzzer.ByteFlipDecorator
+            batch=inf
+
+            [fuzz.foo-with-flips.fuzzer.init]
+            outdir=/home/alice/foo-old-bugs/
+
+            [fuzz.foo-with-flips.fuzzer.decorate(0)]
+            frequency=100
+            min_byte=0
+            max_byte=255
+    """
+
+    def decorator(self, frequency, min_byte='32', max_byte='126', **kwargs):
+        frequency = int(frequency)
+        min_byte = int(min_byte)
+        max_byte = int(max_byte)
+
+        def wrapper(fn):
+            def filter(*args, **kwargs):
+                test = fn(*args, **kwargs)
+                if not test:
+                    return None
+
+                test = bytearray(test)
+                for pos in random.sample(range(len(test)), math.ceil(len(test) / frequency)):
+                    test[pos] = random.randint(min_byte, max_byte)
+
+                return bytes(test)
+
+            return filter
+        return wrapper
---
+++ tests/fuzzer/test_byte_flip_decorator.py
@@ -0,0 +1,47 @@
+# Copyright (c) 2017 Renata Hodovan, Akos Kiss.
+#
+# Licensed under the BSD 3-Clause License
+# <LICENSE.rst or https://opensource.org/licenses/BSD-3-Clause>.
+# This file may not be copied, modified, or distributed except
+# according to those terms.
+
+import inspect
+import pytest
+
+import fuzzinator
+
+from common_fuzzer import mock_exhausted_fuzzer, MockRepeatingFuzzer
+
+
+@pytest.mark.parametrize('fuzzer, fuzzer_init_kwargs, dec_kwargs, exp_flip_cnt', [
+    (mock_exhausted_fuzzer, None, {'frequency': '100'}, None),
+
+    (MockRepeatingFuzzer, {'test': b'\x00\x01\x02\x03\x04\x05\x06\x07\x08\x09\x0A\x0B\x0C\x0D\x0E\x0F', 'n': 10}, {'frequency': '16'}, 1),
+    (MockRepeatingFuzzer, {'test': b'\x10\x11\x12\x13\x14\x15\x16\x17\x18\x19\x1A\x1B\x1C\x1D\x1E\x1F', 'n': 10}, {'frequency': '16'}, 1),
+    (MockRepeatingFuzzer, {'test': b'\x7F\x7F\x7F\x7F\x7F\x7F\x7F\x7F\x7F\x7F\x7F\x7F\x7F\x7F\x7F\x7F', 'n': 10}, {'frequency': '16'}, 1),
+
+    (MockRepeatingFuzzer, {'test': b'\x7F\x7F\x7F\x7F\x7F\x7F\x7F\x7F\x7F\x7F\x7F\x7F\x7F\x7F\x7F\x7F', 'n': 10}, {'frequency': '1'}, 16),
+    (MockRepeatingFuzzer, {'test': b'\x7F\x7F\x7F\x7F\x7F\x7F\x7F\x7F\x7F\x7F\x7F\x7F\x7F\x7F\x7F\x7F', 'n': 10}, {'frequency': '4'}, 4),
+    (MockRepeatingFuzzer, {'test': b'\x7F\x7F\x7F\x7F\x7F\x7F\x7F\x7F\x7F\x7F\x7F\x7F\x7F\x7F\x7F\x7F', 'n': 10}, {'frequency': '10'}, 2),
+    (MockRepeatingFuzzer, {'test': b'\x7F\x7F\x7F\x7F\x7F\x7F\x7F\x7F\x7F\x7F\x7F\x7F\x7F\x7F\x7F\x7F', 'n': 10}, {'frequency': '100'}, 1),
+
+    (MockRepeatingFuzzer, {'test': b'abc', 'n': 10}, {'frequency': '3', 'max_byte': '95'}, 1),
+    (MockRepeatingFuzzer, {'test': b'ABC', 'n': 10}, {'frequency': '3', 'min_byte': '96'}, 1),
+
+    (MockRepeatingFuzzer, {'test': b' ~', 'n': 10}, {'frequency': '1', 'min_byte': '33', 'max_byte': '125'}, 2),
+    (MockRepeatingFuzzer, {'test': b'  ', 'n': 10}, {'frequency': '2', 'min_byte': '32', 'max_byte': '32'}, 0),
+])
+def test_byte_flip_decorator(fuzzer, fuzzer_init_kwargs, dec_kwargs, exp_flip_cnt):
+    fuzzer = fuzzinator.fuzzer.ByteFlipDecorator(**dec_kwargs)(fuzzer)
+    if inspect.isclass(fuzzer):
+        fuzzer = fuzzer(**fuzzer_init_kwargs)
+
+    index = 0
+    while True:
+        test = fuzzer(index=index)
+        if test is None:
+            break
+
+        orig_test = fuzzer_init_kwargs['test']
+        flips = [i for i in range(min(len(orig_test), len(test))) if orig_test[i] != test[i]]
+        assert len(flips) == exp_flip_cnt

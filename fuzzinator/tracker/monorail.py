a97d49c7528fabb5bcb72af43a7f704ac44d421d
Renata Hodovan <reni@inf.u-szeged.hu>
2018-03-27 15:36:22 +0200

    Fix query value in the monorail tracker.

--- fuzzinator/tracker/monorail.py
+++ fuzzinator/tracker/monorail.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2017 Renata Hodovan, Akos Kiss.
+# Copyright (c) 2017-2018 Renata Hodovan, Akos Kiss.
 #
 # Licensed under the BSD 3-Clause License
 # <LICENSE.rst or https://opensource.org/licenses/BSD-3-Clause>.
@@ -38,7 +38,7 @@ class MonorailReport(BaseTracker):
 
     def find_issue(self, issue):
         return self.monorail.issues().list(projectId=self.project_id, can='open',
-                                           q=issue['cond'].decode('utf-8', errors='ignore')).execute().get('items', [])
+                                           q=issue['id'].decode('utf-8', errors='ignore')).execute().get('items', [])
 
     def report_issue(self, title, body):
         return self.monorail.issues().insert(projectId=self.project_id,

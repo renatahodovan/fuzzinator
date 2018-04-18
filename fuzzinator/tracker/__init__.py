ee21d397c99f81699d615c65e7458ff35e8f0177
Renata Hodovan <reni@inf.u-szeged.hu>
2018-04-18 10:18:14 +0200

    Publish BaseTracker.
    Publishing BaseTracker makes possible to define a template for
    displaying the found issues in the viewer.

--- fuzzinator/tracker/__init__.py
+++ fuzzinator/tracker/__init__.py
@@ -1,16 +1,18 @@
-# Copyright (c) 2016-2017 Renata Hodovan, Akos Kiss.
+# Copyright (c) 2016-2018 Renata Hodovan, Akos Kiss.
 #
 # Licensed under the BSD 3-Clause License
 # <LICENSE.rst or https://opensource.org/licenses/BSD-3-Clause>.
 # This file may not be copied, modified, or distributed except
 # according to those terms.
 
+from .base import BaseTracker
 from .bugzilla import BugzillaReport
 from .github import GithubReport
 from .monorail import MonorailReport
 
 
 __all__ = [
+    'BaseTracker',
     'BugzillaReport',
     'GithubReport',
     'MonorailReport',

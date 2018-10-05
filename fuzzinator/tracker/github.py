414fa856aad743a5e6acec89f7fb6844a5df070f
Renata Hodovan <reni@inf.u-szeged.hu>
2018-10-05 13:34:55 +0200

    Remove unused import from GitHubReport.

--- fuzzinator/tracker/github.py
+++ fuzzinator/tracker/github.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2016-2017 Renata Hodovan, Akos Kiss.
+# Copyright (c) 2016-2018 Renata Hodovan, Akos Kiss.
 #
 # Licensed under the BSD 3-Clause License
 # <LICENSE.rst or https://opensource.org/licenses/BSD-3-Clause>.
@@ -15,8 +15,6 @@ try:
     from github import Github, BadCredentialsException
 except ImportError:
     pass
-
-from string import Template
 
 from .base import BaseTracker
 

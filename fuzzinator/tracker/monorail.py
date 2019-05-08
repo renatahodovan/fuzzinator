a9bdf584f355d994e058e71ea938f7abca18bb1b
Renata Hodovan <reni@inf.u-szeged.hu>
2019-05-08 11:56:59 +0200

    Add missing execute call to the Monorail issue reporting

--- fuzzinator/tracker/monorail.py
+++ fuzzinator/tracker/monorail.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2017-2018 Renata Hodovan, Akos Kiss.
+# Copyright (c) 2017-2019 Renata Hodovan, Akos Kiss.
 #
 # Licensed under the BSD 3-Clause License
 # <LICENSE.rst or https://opensource.org/licenses/BSD-3-Clause>.
@@ -41,7 +41,7 @@ class MonorailTracker(BaseTracker):
     def report_issue(self, title, body):
         return self.monorail.issues().insert(projectId=self.project_id,
                                              body=dict(summary=title,
-                                                       description=body))
+                                                       description=body)).execute()
 
     def __call__(self, issue):
         pass

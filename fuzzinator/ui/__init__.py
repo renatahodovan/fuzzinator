cc26f3460cf470a4bb4ac2e48a09f6dafbf4f827
Akos Kiss <akiss@inf.u-szeged.hu>
2020-03-10 16:25:04 +0100

    Don't import UI submodules in ui/__init__.py
    This avoids the initialization of unnecessary modules and packages.

--- fuzzinator/ui/__init__.py
+++ fuzzinator/ui/__init__.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2016-2018 Renata Hodovan, Akos Kiss.
+# Copyright (c) 2016-2020 Renata Hodovan, Akos Kiss.
 #
 # Licensed under the BSD 3-Clause License
 # <LICENSE.rst or https://opensource.org/licenses/BSD-3-Clause>.
@@ -6,6 +6,3 @@
 # according to those terms.
 
 from .arg_parser import build_parser, process_args
-
-from . import cli
-from . import tui

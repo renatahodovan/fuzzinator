8047558f798bf9f414e9a1a2d1cdd254e6473e54
Paulo Matos <pmatos@linki.tools>
2020-12-02 17:01:05 +0100

    Adds id key to issue for validation (#30)
    Fixes #29

--- fuzzinator/job/validate_job.py
+++ fuzzinator/job/validate_job.py
@@ -40,6 +40,8 @@ class ValidateJob(CallJob):
 
         if issue:
             issue['test'] = self.issue['test']
+
+            self.ensure_id(issue)
             if issue['id'] == self.issue['id'] and not self.issue.get('invalid'):
                 self.db.update_issue_by_oid(self.issue['_id'], issue)
                 return True, new_issues

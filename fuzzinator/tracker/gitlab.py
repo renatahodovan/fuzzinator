be006cc4487c1c3e4d95b03b55a3a2a451d9d494
Hodovan Renata <reni@inf.u-szeged.hu>
2021-05-26 01:25:36 +0200

    Various fixes in the Gitlab tracker
    1) The issue search query of the Gitlab tracker returns a list of
       dictionary items describing the matching issue entries and not
       a list of RESTObject items as the previous implementation wrongly
       presumed. Furthermore, it wrongly set the `title` field of the
       result dictionary to the description value of the issue.
       These mistakes are fixed now.
    2) The result dictionary of a Gitlab report falsely set its `title`
       field to the description of the issue report. Although, this
       information is unused here, but it's better to have it correct.
    
    Fixes #36.

--- fuzzinator/tracker/gitlab.py
+++ fuzzinator/tracker/gitlab.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2019 Renata Hodovan, Akos Kiss.
+# Copyright (c) 2019-2021 Renata Hodovan, Akos Kiss.
 #
 # Licensed under the BSD 3-Clause License
 # <LICENSE.rst or https://opensource.org/licenses/BSD-3-Clause>.
@@ -19,14 +19,14 @@ class GitlabTracker(BaseTracker):
 
     def find_issue(self, query):
         issues = [issue for issue in self.project.search('issues', query) if issue['state'] == 'opened']
-        return [{'id': issue.attributes['id'],
-                 'title': issue.attributes['description'],
-                 'url': issue.attributes['web_url']} for issue in issues]
+        return [{'id': issue['id'],
+                 'title': issue['title'],
+                 'url': issue['web_url']} for issue in issues]
 
     def report_issue(self, title, body):
         new_issue = self.project.issues.create(dict(title=title, description=body))
         return {'id': new_issue.attributes['id'],
-                'title': new_issue.attributes['description'],
+                'title': new_issue.attributes['title'],
                 'url': new_issue.attributes['web_url']}
 
     def __call__(self, issue):

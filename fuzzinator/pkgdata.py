ccaa998a6484891da921147d288c85812351a1be
Akos Kiss <akiss@inf.u-szeged.hu>
2021-08-24 23:39:43 +0200

    Use importlib.metadata to retrieve package version and other metadata
    (Use importlib_metadata backport for <py38.)

--- fuzzinator/pkgdata.py
+++ fuzzinator/pkgdata.py
@@ -1,19 +1,20 @@
-# Copyright (c) 2016-2019 Renata Hodovan, Akos Kiss.
+# Copyright (c) 2016-2021 Renata Hodovan, Akos Kiss.
 #
 # Licensed under the BSD 3-Clause License
 # <LICENSE.rst or https://opensource.org/licenses/BSD-3-Clause>.
 # This file may not be copied, modified, or distributed except
 # according to those terms.
 
-from email import message_from_string
-from pkg_resources import get_distribution
+try:
+    from importlib import metadata
+except ImportError:
+    import importlib_metadata as metadata
 
 
-dist = get_distribution(__package__)
-meta = message_from_string(dist.get_metadata(dist.PKG_INFO))
+dist = metadata.distribution(__package__)
 
-__pkg_name__ = dist.project_name
+__pkg_name__ = dist.metadata['Name']
 __version__ = dist.version
-__author__ = meta['Author']
-__author_email__ = meta['Author-email']
-__url__ = meta['Home-page']
+__author__ = dist.metadata['Author']
+__author_email__ = dist.metadata['Author-email']
+__url__ = dist.metadata['Home-page']

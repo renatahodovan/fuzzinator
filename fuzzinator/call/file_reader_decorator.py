9ea9b7498bf3e9c5758c2d3d549df9345a86240e
Renáta Hodován <reni@inf.u-szeged.hu>
2022-11-05 23:25:45 +0100

    Extend fuzzinator.call.FileReaderDecorator with cleanup functionality (#49)

--- fuzzinator/call/file_reader_decorator.py
+++ fuzzinator/call/file_reader_decorator.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2017-2021 Renata Hodovan, Akos Kiss.
+# Copyright (c) 2017-2022 Renata Hodovan, Akos Kiss.
 #
 # Licensed under the BSD 3-Clause License
 # <LICENSE.rst or https://opensource.org/licenses/BSD-3-Clause>.
@@ -7,17 +7,23 @@
 
 import os
 
+from ..config import as_bool
 from .call_decorator import CallDecorator
 
 
 class FileReaderDecorator(CallDecorator):
     """
     Decorator for SUTs that take input as a file path: saves the content of
-    the failing test case.
+    the failing test case and optionally removes the file.
 
     Moreover, the issue (if any) is also extended with the new ``'filename'``
     property containing the name of the test case (as received in the ``test``
     argument).
+
+    **Optional parameter of the decorator:**
+
+      - ``cleanup``: Boolean to enable the removal of the loaded file
+        (default: False).
 
     **Example configuration snippet:**
 
@@ -30,10 +36,13 @@ class FileReaderDecorator(CallDecorator):
             [sut.foo.call]
             # assuming that foo takes one file as input specified on command line
             command=/home/alice/foo/bin/foo {test}
+
+            [sut.foo.decorate(0)]
+            cleanup=True
     """
 
-    def __init__(self, **kwargs):
-        pass
+    def __init__(self, *, cleanup=False, **kwargs):
+        self.cleanup = as_bool(cleanup)
 
     def call(self, cls, obj, *, test, **kwargs):
         issue = super(cls, obj).__call__(test=test, **kwargs)
@@ -42,5 +51,8 @@ class FileReaderDecorator(CallDecorator):
             with open(test, 'rb') as f:
                 issue['test'] = f.read()
             issue['filename'] = os.path.basename(test)
+
+        if self.cleanup:
+            os.remove(test)
 
         return issue
--- tests/call/test_file_reader_decorator.py
+++ tests/call/test_file_reader_decorator.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2017-2021 Renata Hodovan, Akos Kiss.
+# Copyright (c) 2017-2022 Renata Hodovan, Akos Kiss.
 #
 # Licensed under the BSD 3-Clause License
 # <LICENSE.rst or https://opensource.org/licenses/BSD-3-Clause>.
@@ -7,22 +7,29 @@
 
 import os
 import pytest
+import shutil
 
 import fuzzinator
 
 from common_call import blinesep, resources_dir, MockAlwaysFailCall, MockNeverFailCall
 
 
+@pytest.mark.parametrize('dec_kwargs', [
+    {},
+    {'cleanup': False},
+    {'cleanup': True},
+])
 @pytest.mark.parametrize('call_init_kwargs, call_kwargs', [
-    ({'init_foo': 'init_bar'}, {'test': os.path.join(resources_dir, 'mock_tests', 'bar.txt')})
+    ({'init_foo': 'init_bar'}, {'test': os.path.join(resources_dir, 'mock_tests', 'bar.txt')}),
 ])
 @pytest.mark.parametrize('call_class, exp', [
     (MockAlwaysFailCall, {'init_foo': 'init_bar', 'test': b'bar' + blinesep, 'filename': 'bar.txt'}),
     (MockNeverFailCall, None),
 ])
-def test_file_reader_decorator(call_class, call_init_kwargs, call_kwargs, exp):
-    call_class = fuzzinator.call.FileReaderDecorator()(call_class)
+def test_file_reader_decorator(call_class, call_init_kwargs, call_kwargs, dec_kwargs, exp, tmpdir):
+    call_class = fuzzinator.call.FileReaderDecorator(**dec_kwargs)(call_class)
     call = call_class(**call_init_kwargs)
-
+    test_path = shutil.copy(call_kwargs['test'], str(tmpdir))
     with call:
-        assert call(**call_kwargs) == exp
+        assert call(**dict(call_kwargs, test=test_path)) == exp
+        assert dec_kwargs.get('cleanup', False) == (not os.path.exists(test_path))
